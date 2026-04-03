4e3a628a513e3a9ffe555ef1d894d4c0ede5d2a9
varshithdyagala <153904855+varshithdyagala@users.noreply.github.com>
2026-04-03 11:53:17 +0530

    AI generated: Build a Python script that prints Fibonacci number

--- build-a-python-script-that-pri.py
+++ build-a-python-script-that-pri.py
@@ -1,8 +1,19 @@
-fib_sequence = [0, 1]
+def fibonacci(n):
+    if n <= 0:
+        return []
+    elif n == 1:
+        return [0]
+    elif n == 2:
+        return [0, 1]
+    
+    fib_sequence = [0, 1]
+    for i in range(2, n):
+        next_value = fib_sequence[-1] + fib_sequence[-2]
+        fib_sequence.append(next_value)
+        print(next_value)  # Print each Fibonacci number as it is generated
+    
+    return fib_sequence
 
-for i in range(2, 10):
-    next_value = fib_sequence[-1] + fib_sequence[-2]
-    fib_sequence.append(next_value)
-
-for number in fib_sequence:
-    print(number)
+if __name__ == "__main__":
+    n = 10  # Number of Fibonacci numbers to generate
+    fibonacci(n)
